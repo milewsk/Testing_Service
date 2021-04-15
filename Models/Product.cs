cc95f57564046b07c73fe6938c08af310d2e5e81
Konrad Milewski <konradmilewski2000@interia.eu>
2021-04-15 12:49:16 +0200

    Dodanie pliku, klasa product

---
+++ Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Testing_Service.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+
+        [JsonPropertyName("maker")]
+        public string Maker { get; set; }
+
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("descripton")]
+        public string Description { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+        
+           
+        
+    }
+}
